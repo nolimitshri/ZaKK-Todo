e1e3bf33bdc89413ebb4359efc631ff4e77a0789
nolimitshri <shri04052003@gmail.com>
2022-01-12 12:55:44 +0530

    Updated redirect URI for Google OAuth 2

--- config/passport-oauth-google.js
+++ config/passport-oauth-google.js
@@ -8,7 +8,7 @@ module.exports = function(passport){
         new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/dashboard"
+        callbackURL: "https://zakk-todoapp.herokuapp.com/auth/google/dashboard"
       },
       function(accessToken, refreshToken, profile, cb) {
         User.findOne({
